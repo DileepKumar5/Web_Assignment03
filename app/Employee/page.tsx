a982403f603dc3e5005eb20251b6efa172edf9a8
DileepKumar5 <dileepkumar5062002@gmail.com>
2024-04-28 22:04:30 +0500

    assignment 03

--- app/Employee/page.tsx
+++ app/Employee/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
  export const Employeepage = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-    <h1 className='text-5xl font-bold text-center mb-8 text-blue-800'>
+    <h1 className='text-5xl font-bold text-center mb-8 text-blue-900'>
         CRUD Operations Using Next JS 14, Prisma & PostgreSQL
     </h1>
 
